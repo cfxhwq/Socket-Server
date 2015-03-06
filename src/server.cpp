f0689aa7f7a4e57cc7a92313628bf055c43fde5f
cfxhwq <xmhwq@sina.com>
2015-03-06 17:03:37 +0800

    C

--- src/server.cpp
+++ src/server.cpp
@@ -43,16 +43,21 @@ int main() {
 		perror("setsockopt() error ");
 		return 1;
 	}
-	if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
-	{
-		perror("setsockopt() error ");
-		return 1;
-	}
+//	int opt1 = SO_REUSEPORT;
+//	if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEPORT, &opt1, sizeof(opt1)) == -1)
+//	{
+//		perror("setsockopt() error ");
+//		return 1;
+//	}
 	if (bind(listenfd, (struct sockaddr *) &server, sizeof(server)) == -1) {
 		cout << server.sin_addr.s_addr << endl;
 		perror("bind() error");
 		return 1;
 	}
+	addrlen = sizeof(server);
+	int a = getsockname(listenfd, (struct sockaddr *) &server, &addrlen);
+	cout << a << endl;
+
 	cout << "bind finish" << endl;
 	if (listen(listenfd, BACKLOG) == -1) {
 		perror("listen() error");
@@ -61,12 +66,18 @@ int main() {
 	cout << "listen finish" << endl;
 	addrlen = sizeof(client);
 
-	if ((connectfd = accept(listenfd, (struct sockaddr *) &client, &addrlen))) {
-		perror("accept() error\n");
+	if ((connectfd = accept(listenfd, (struct sockaddr *) &client, &addrlen))
+			== -1) {
+		perror("accept() error");
 		return 1;
 	}
 	cout << "accept finish" << endl;
 	printf("Client's ip is %s,port is %d\n", inet_ntoa(client.sin_addr),
 			htons(client.sin_port));
+
+	send(connectfd, "WELCOME!\n",9, 0);
+
+	close(connectfd);
+	close(listenfd);
 	return 0;
 }
